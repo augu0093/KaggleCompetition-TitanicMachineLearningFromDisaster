c39184b76da61976a74dcc8e69829e895e6630f5
augu0093 <augu0093@gmail.com>
2020-07-06 22:21:31 +0200

    Will optimize KNN, DT and work on XGB, then wrap up

---
+++ optimizationKNN.py
@@ -0,0 +1,10 @@
+"""
+This script is a low-effort tuning of K-Nearest-Neighbor model.
+@AugustSemrau
+"""
+
+from dataLoader import dataLoader
+from sklearn.neighbors import KNeighborsClassifier
+
+
+
